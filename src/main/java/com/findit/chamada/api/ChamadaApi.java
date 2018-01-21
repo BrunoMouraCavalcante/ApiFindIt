4f6f46683e0fa9f0fd327928384fee3bab7de6e5
BrunoMoura <bmc_brunomc@hotmail.com>
2018-01-21 11:43:16 -0200

    api chamada

---
+++ src/main/java/com/findit/chamada/api/ChamadaApi.java
@@ -0,0 +1,10 @@
+package com.findit.chamada.api;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/dota2api/player")
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+public class ChamadaApi {
+}
